39db8b4a75da2a4b7af4c8346e3d9e0b78398a78
Glitch (grandiose-awesome-plate) <none>
2021-02-12 10:20:19 +0000

    😜🖇 Checkpoint ./index.js:24866363/3103

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const jsonfile = require('jsonfile')
 var all_events = '../data/tweets.json';
 
 var prefix_api = "/api/v1";
+var month = process.env.MONTH;
+var year = process.env.YEAR;
 
 
 /**
@@ -23,9 +25,14 @@ fastify.get('/', async (request, reply) => {
 })
 
 // API route - "events"
-fastify.get(prefix_api + '/events', async (request, reply) => {
-  var result = [];
-  return result.push(getAllEvents);
+fastify.get(prefix_api + '/event/date=1-' + month + '-' + year , async (request, reply) => {
+  return {
+      "date" : "01.03.2021",
+      "title" : "The Moon at aphelion",
+      "summary" : "The Moon's monthly orbit around the Earth will carry it to its furthest point from the Sun – its aphelion – at a distance of 0.9930 AU from the Sun.",
+      "url" : "https://in-the-sky.org/news.php?id=20210301_08_100",
+      "imageUrl" : "https://in-the-sky.org/image.php?style=icon&img=imagedump/moon/the_moon_at_aphelion.jpg"
+  }
 })
 
 // Run the server!
